e968a0a676f7ddbba853af11fc4d1ad0cd5f1d95
Anil Pavan <appkumark@yahoo.com>
2019-10-31 05:42:30 +0530

    fourth commit

--- src/context.js
+++ src/context.js
@@ -13,6 +13,9 @@ class ProductProvider extends Component {
       cart: [],
       modalOpen: false,
       modalProduct: detailProduct,
+      cartSubTotal: 0,
+      cartTax: 0,
+      cartTotal: 0
    };
    componentDidMount() {
       this.setProducts();
@@ -66,8 +69,20 @@ class ProductProvider extends Component {
       })
    }
 
+   increment = (id) => {
+      console.log('this is increment method');
+   }
 
+   decrement = (id) => {
+      console.log('this is decrement method');
+   }
 
+   removeItem = (id) => {
+      console.log('item removed');
+   }
+   clearCart = () => {
+      console.log('cart was cleared');
+   }
    render() {
       return (
          <ProductContext.Provider value={{
@@ -75,7 +90,11 @@ class ProductProvider extends Component {
             handleDetail: this.handleDetail,
             addToCart: this.addToCart,
             openModal: this.openModal,
-            closeModal: this.closeModal
+            closeModal: this.closeModal,
+            increment: this.increment,
+            decrement: this.decrement,
+            removeItem: this.removeItem,
+            clearCart: this.clearCart
 
          }}>
             {this.props.children}
